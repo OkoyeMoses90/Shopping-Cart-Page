92c0ee130f57f111f1cd50d205db6f0f955723b3
Ekene Okoye <ekeneokoye@Ekenes-MBP.attlocal.net>
2020-06-17 19:59:38 -0500

    adding files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import NavBar from './components/navbar';
+import Counters from "./components/counters";
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  state = {
+    counters: [
+      { id: 1, count: 0},
+      { id: 2, count: 0},
+      { id: 3, count: 0},
+      { id: 4, count: 0}
+    ]
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {...counter};
+    counters[index].count++;
+    this.setState({counters});
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.count = 0;
+      return c;
+    });
+    this.setState({counters});
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters});
+  };
+
+  render() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <NavBar totalCounters = {this.state.counters.filter(c => c.count > 0).length}/>
+      <main className = "container" >
+        <Counters
+          counters = {this.state.counters}
+          onReset = {this.handleReset}
+          onDelete = {this.handleDelete}
+          onIncrement = {this.handleIncrement}
+        />
+      </main>
+    </React.Fragment>
+
+    );
+  }
 }
 
 export default App;
